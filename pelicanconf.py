975b7f7b1f29e25fe677239d4bac50781fb94a49
Petr Horáček <phoracek@redhat.com>
2015-10-15 19:10:32 +0200

    initial config of pelican sources

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Petr Horáček'
+SITENAME = u'Petr Horáček'
+SITEURL = 'blog.phoracek.cz'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Prague'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
